cdf51bc92ba751e157c5f40cd3a2d3c974599138
Diwa Perkasa <diwa.perkasa@gmail.com>
2024-05-25 15:43:41 +0700

    add dfp function

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-add_action( 'after_setup_theme', 'crb_load' );
-add_action( 'carbon_fields_register_fields', 'add_custom_field_action' );
+add_action('after_setup_theme', 'crb_load');
+add_action('carbon_fields_register_fields', 'add_custom_field_action');
 
 function crb_load()
 {
-    require_once( __DIR__ . '/Lib/vendor/autoload.php' );
+    require_once(__DIR__ . '/Lib/vendor/autoload.php');
     // Initialize Carbon
     \Carbon_Fields\Carbon_Fields::boot();
     // Initialize Timber.
@@ -32,6 +32,40 @@ function add_custom_field_action()
 function rrsg_render(string $path, array $data = [])
 {
     $context = \Timber\Timber::context($data);
+    $templateData = [
+        'data' => $context,
+        'dfpTarget' => get_targets(),
+    ];
 
-    return \Timber\Timber::compile("page/{$path}", $context);
+    return \Timber\Timber::compile("page/{$path}", $templateData);
+}
+
+// DFP Tags
+function get_targets()
+{
+    global $post;
+    $targets = [];
+
+    if (is_home() || is_front_page() || is_page(array('newsletter', 'print-and-digital-subscription'))) {
+        $targets[] = 'home';
+    } elseif (is_singular(array('post', 'features'))) {
+        $categories = wp_get_object_terms($post->ID, 'category');
+        if (!empty($categories)) {
+            foreach ($categories as $category)
+                $targets[] = $category->slug;
+        }
+        $targets[] = array_push($targets, "$post->ID");
+    } elseif (is_author()) {
+        $targets[] = 'home';
+    } elseif (is_category()) {
+        $term = get_queried_object();
+        if ($term->parent > 0) {
+            $term      = get_term_by('id', $term->parent, 'category');
+            $targets[] = $term->slug;
+        } else {
+            $targets[] = $term->slug;
+        }
+    }
+
+    return $targets;
 }
---
+++ page.php
@@ -0,0 +1,3 @@
+<?php
+
+echo rrsg_render('home.twig');
