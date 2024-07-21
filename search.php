a6aa211f97c182974cecd1fdf489dab6878d31f5
Diwa Perkasa <diwa.perkasa@gmail.com>
2024-07-21 20:46:10 +0700

    add feature page

--- search.php
+++ search.php
@@ -1,15 +1,3 @@
 <?php
 
-$articles = [];
-
-while ( have_posts() )
-{
-    the_post();
-    $articles[] = get_post();
-}
-
-$data = [
-    'articles' => $articles
-];
-
-echo rrsg_render('search.twig', $data);
+echo rrsg_render('search.twig');
---
+++ single-features.php
@@ -0,0 +1,5 @@
+<?php
+
+add_filter( 'post_gallery', 'rrsg_custom_post_gallery' , 10, 3);
+
+echo rrsg_render('single-features.twig');
