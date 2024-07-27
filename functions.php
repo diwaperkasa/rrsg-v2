1bb2c0fc6aebdd6a9d2ee16bbdde63c1651b6a60
Diwa Perkasa <diwa.perkasa@gmail.com>
2024-07-27 15:20:09 +0700

    add newsletters and subscribe function

--- functions.php
+++ functions.php
@@ -6,6 +6,16 @@ use Carbon_Fields\Field;
 add_action('after_setup_theme', 'crb_load');
 add_action('carbon_fields_register_fields', 'add_custom_field_action');
 
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+function my_acf_json_save_point($path)
+{
+
+	$path = get_stylesheet_directory() . '/acf-json';
+
+	return $path;
+}
+
 //Add Style
 function theme_enqueue_styles()
 {
