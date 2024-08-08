a2ce9877ddcdee52c09ced6d126c43264f9a680f
Diwa Perkasa <diwa.perkasa@gmail.com>
2024-08-08 14:54:23 +0700

    update mobile menu

--- functions.php
+++ functions.php
@@ -697,3 +697,65 @@ function get_desktop_menu()
 
     return $menu_list;
 }
+
+function get_mobile_menu()
+{
+    $menu_name = 'primary-menu';
+    $menu_list = '<ul class="nav side-nav flex-column">' . "\n";
+
+    if ($menu_items = wp_get_nav_menu_items($menu_name)) {
+        $count = 0;
+        $submenu = false;
+        $parent_id = 0;
+        $previous_item_has_submenu = false;
+
+        foreach ((array) $menu_items as $key => $menu_item) {
+            $title = $menu_item->title;
+            $url = $menu_item->url;
+            // check if it's a top-level item
+            if ($menu_item->menu_item_parent == 0) {
+                $parent_id = $menu_item->ID;
+                // write the item but DON'T close the A or LI until we know if it has children!
+                $menu_list .= "\t" . '<li class="nav-item"><a class="nav-link  fs-1" href="' . $url . '">' . $title;
+            }
+            // if this item has a (nonzero) parent ID, it's a second-level (child) item
+            else {
+                if (!$submenu) { // first item
+                    // add the dropdown arrow to the parent
+                    $menu_list .= '</a>' . "\n";
+                    // start the child list
+                    $submenu = true;
+                    $previous_item_has_submenu = true;
+                    $menu_list .= "\t\t" . '<ul class="ms-3 nav flex-column">' . "\n";
+                }
+
+                $menu_list .= "\t\t\t" . '<li class="nav-item">';
+                $menu_list .= '<a class="nav-link  fs-2" href="' . $url . '">' . $title . '</a>';
+                $menu_list .= '</li>' . "\n";
+                // if it's the last child, close the submenu code
+                if (isset($menu_items[$count + 1]) && $menu_items[$count + 1]->menu_item_parent != $parent_id && $submenu) {
+                    $menu_list .= "\t\t" . '</ul></li>' . "\n";
+                    $submenu = false;
+                }
+            }
+            // close the parent (top-level) item
+            if (empty($menu_items[$count + 1]) || $menu_items[$count + 1]->menu_item_parent != $parent_id) {
+                if ($previous_item_has_submenu) {
+                    // the a link and list item were already closed
+                    $previous_item_has_submenu = false; //reset
+                } else {
+                    // close a link and list item
+                    $menu_list .= "\t" . '</a></li>' . "\n";
+                }
+            }
+
+            $count++;
+        }
+    } else {
+        $menu_list .= '<!-- no list defined -->';
+    }
+
+    $menu_list .= "\t" . '</ul>' . "\n";
+
+    return $menu_list;
+}
